8865a4c924fd0034285d167c444a184ee0c2205e
username <useremail@qq.com>
2024-03-04 00:49:01 +0800

    111

--- CS/__init__.py
+++ CS/__init__.py
@@ -2,38 +2,39 @@
 print("Hello AS!")
 
 print("Hello World")
+
+import random  
+import time
 from ascript.android import system
-
-
-#system.open("快手免费小说")
-
 from ascript.android.node import Selector
+from ascript.android.system import Device
+from ascript.android import action
+
+system.open("快手免费小说")
+time.sleep(11)
 
 
-# node =  Selector().text("^书架$")
+node =  Selector().text("^书架$")
 
-# if node.find():
-#     node.parent(1).click().find()
-#     print('找到书架')
+if node.find():
+    node.parent(1).click().find()
+    time.sleep(5)
+    print('找到书架')
     
-# else:
-#     print('没有找到任何控件')
+else:
+    print('没有找到任何控件')
 
-#Selector().id("com.kuaishou.kgx.novel:id/read_info").text("未读").parent(1).click().find()
+Selector().id("com.kuaishou.kgx.novel:id/read_info").text("未读").parent(1).click().find()
+time.sleep(5)
+display = Device.display()
+x,y=display.widthPixels,display.heightPixels
 
-#click(,0.8*y)
+for i in range(1, 1511):  
 
-# 获取屏幕信息
-# from ascript.android.system import Device
-# display = Device.display()
-# x,y=display.widthPixels,display.heightPixels
-# from ascript.android import action
-# for i in range(1, 1111):  
+    action.click(0.95*x,0.8*y)
 
-#     action.click(0.95*x,0.8*y)
-#     import random  
-#     import time
-#     time.sleep(random.randint(11, 24))
+    time.sleep(random.randint(11, 24))
+
 
 # node =  Selector().text("^今日已达上限$")
 
@@ -42,3 +43,25 @@ from ascript.android.node import Selector
     
 # else:
 #     print('没有找到任何控件')
+
+
+# node =  Selector().text("看视频领金币")
+
+# if node.find():
+#     node.click().find()
+#     time.sleep(5)
+#     print('找到看视频领金币')
+    
+# else:
+#     print('没有看视频领金币')
+
+
+# node =  Selector().id("com.kuaishou.kgx.novel:id/video_close_icon")
+
+# if node.find():
+#     node.click().find()
+#     time.sleep(5)
+#     print('找到看视频领金币')
+    
+# else:
+#     print('没有看视频领金币')
