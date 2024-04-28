726062bad2178a4d27ab1e5f4da5302acacb1913
username <useremail@qq.com>
2024-04-28 13:50:07 +0800

    11

---
+++ A2/__init__.py
@@ -0,0 +1,14 @@
+# __init__.py 为初始化加载文件
+
+#导入-资源路径规划库
+from airscript.system import R
+
+#导入-节点检索库
+from airscript.node import Selector
+
+#导入-屏幕检索库
+from airscript.screen import FindColors # 找色
+from airscript.screen import CompareColors # 比色
+from airscript.screen import FindImages # 找图
+from airscript.screen import Ocr # 文字识别
+
