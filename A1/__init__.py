8e12d9e16ac1ac3dd4d3219024091f42f597dd89
username <useremail@qq.com>
2024-09-08 19:58:55 +0800

    111

--- A1/__init__.py
+++ A1/__init__.py
@@ -8,8 +8,3 @@ import time
 # 写一个循环,每3秒执行一次 ,执行100秒 
 for i in range(32):
     slide(200,1200,300,400,900)
-<<<<<<< HEAD
-    time.sleep(3)
-=======
-    time.sleep(7)
->>>>>>> 2753cc1fb12d39bb3bfbd1eb9978ceb2e5c465a8
